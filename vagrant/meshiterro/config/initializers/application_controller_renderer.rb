ef2667241f5cb3f8bcdd5944aa6c0b3f2305a715
unknown <hannzaimonn@gmail.com>
2020-06-01 14:54:30 +0900

    [Finish] 5-8

--- vagrant/meshiterro/config/initializers/application_controller_renderer.rb
+++ vagrant/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bfc47fc7e69c9875479c6a8c2e564b2541f736b1677c399cd8300cd177dca320f055551ab87c4c098a71759a98dab8fb04ee10a42995d25c13670efd9418332d'
